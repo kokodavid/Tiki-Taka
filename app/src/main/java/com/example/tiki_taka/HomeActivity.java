c026a8f3e14af72538e8f4dfcd979d595c4bdcbe
David mochoge <kingkgdave@gmail.com>
2019-05-10 16:44:11 +0300

    Add list view and arrayAdapter

--- app/src/main/java/com/example/tiki_taka/HomeActivity.java
+++ app/src/main/java/com/example/tiki_taka/HomeActivity.java
@@ -1,13 +1,34 @@
 package com.example.tiki_taka;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView mLocationTextView;
+    private ListView mListView;
+
+
+    private String[] events = new String[] {
+            "Na Iwake", "Color Splash","kingKong pool Party", "Imax Avengers End Game", "Jameson party",
+            "Mother's Day","Mad run","Nairobi Sunset Gt","Nairobi Tech Week","Matter heart run","The Forest hike","Etana Live in Kenya"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mListView = (ListView) findViewById(R.id.locationListView);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,events);
+        mListView.setAdapter(adapter);
+
+
     }
 }
