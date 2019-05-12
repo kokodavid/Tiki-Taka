67e7fede680d560ed54d9e7e7a7c80d565e29140
David mochoge <kingkgdave@gmail.com>
2019-05-12 17:59:39 +0300

    add images

--- app/src/main/java/com/example/tiki_taka/HomeActivity.java
+++ app/src/main/java/com/example/tiki_taka/HomeActivity.java
@@ -1,42 +1,53 @@
 package com.example.tiki_taka;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
 
 public class HomeActivity extends AppCompatActivity {
 
 
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
     private ListView mListView;
     private TextView mAppNameTextView;
-
+    private TextView mLocationTextView;
 
     private String[] events = new String[] {
             "Na Iwake", "Color Splash","kingKong pool Party", "Imax Avengers End Game", "Jameson party",
             "Mother's Day","Mad run","Nairobi Sunset Gt","Nairobi Tech Week","Matter heart run","The Forest hike","Etana Live in Kenya"
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mListView = (ListView) findViewById(R.id.locationListView);
+
         ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,events);
         mListView.setAdapter(adapter);
+
+
 
         mAppNameTextView = (TextView) findViewById(R.id.locationTextView);
         Typeface Windsong = Typeface.createFromAsset(getAssets(),"fonts/Windsong.ttf");
         mAppNameTextView.setTypeface(Windsong);
 
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -49,5 +60,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+
+        };
+
     }
-}
