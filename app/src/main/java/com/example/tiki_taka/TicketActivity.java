7db530032aec6ea7a4510fa9faa2c0a4dc281f5a
David mochoge <kingkgdave@gmail.com>
2019-05-12 20:10:32 +0300

    add image and font

--- app/src/main/java/com/example/tiki_taka/TicketActivity.java
+++ app/src/main/java/com/example/tiki_taka/TicketActivity.java
@@ -1,6 +1,7 @@
 package com.example.tiki_taka;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 public class TicketActivity extends AppCompatActivity {
 
     private TextView mLocationTextView;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,8 +18,13 @@ public class TicketActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ticket);
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+
+        mAppNameTextView = (TextView) findViewById(R.id.locationTextView);
+           Typeface Windsong = Typeface.createFromAsset(getAssets(),"fonts/Amatic-Bold.ttf");
+                    mAppNameTextView.setTypeface(Windsong);
+
           mLocationTextView = (TextView) findViewById(R.id.locationTextView);
-                mLocationTextView.setText("You have successfully a ticket for the Event: " + location );
+                mLocationTextView.setText("You have successfully bought a ticket for the Event: " + location );
     }
     }
 
