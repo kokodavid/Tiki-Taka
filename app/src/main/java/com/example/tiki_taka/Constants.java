8ae4ff1f1050178415065ca731e5110ad5e629ce
David mochoge <kingkgdave@gmail.com>
2019-05-16 11:07:54 +0300

    update changes

--- app/src/main/java/com/example/tiki_taka/Constants.java
+++ app/src/main/java/com/example/tiki_taka/Constants.java
@@ -2,7 +2,7 @@ package com.example.tiki_taka;
 
 public class Constants {
     public static final String EventBriteToken = BuildConfig.EventBriteToken;
-    public static final String EventBriteBASE_URL = "https://www.eventbriteapi.com/v3";
-    public static final String EventBrite_LOCATION_QUERY_PARAMETER = "location";
+    public static final String EVENTBRITEBASE_URL = "https://www.eventbriteapi.com/v3/events/search/";
+    public static final String EventBrite_Events_QUERY_PARAMETER = "event";
 
 }
--- app/src/main/java/com/example/tiki_taka/EventBriteService.java
+++ app/src/main/java/com/example/tiki_taka/EventBriteService.java
@@ -1,4 +1,27 @@
 package com.example.tiki_taka;
 
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class EventBriteService {
-}
+
+        public static void findEvents(String location, Callback callback) {
+            /*the okhttp client sends the request*/
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .build();
+
+            HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.EVENTBRITEBASE_URL).newBuilder();
+            urlBuilder.addQueryParameter(Constants.EventBrite_Events_QUERY_PARAMETER, location);
+            String url = urlBuilder.build().toString();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .header("Authorization", Constants.EventBriteToken)
+                    .build();
+
+        }
+
+    }
+
