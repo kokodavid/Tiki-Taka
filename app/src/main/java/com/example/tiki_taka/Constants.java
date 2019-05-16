716fe9aa41a7170bc9c709c2504ab995675c424c
David mochoge <kingkgdave@gmail.com>
2019-05-16 10:35:41 +0300

    add Event Brite token

--- app/src/main/java/com/example/tiki_taka/Constants.java
+++ app/src/main/java/com/example/tiki_taka/Constants.java
@@ -1,4 +1,8 @@
 package com.example.tiki_taka;
 
 public class Constants {
+    public static final String EventBriteToken = BuildConfig.EventBriteToken;
+    public static final String EventBriteBASE_URL = "https://www.eventbriteapi.com/v3";
+    public static final String EventBrite_LOCATION_QUERY_PARAMETER = "location";
+
 }
