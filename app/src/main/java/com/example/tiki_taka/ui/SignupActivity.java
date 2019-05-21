ff64ad93973d7d9f1d846bad992770c8ff04ddcb
David mochoge <kingkgdave@gmail.com>
2019-05-21 16:40:05 +0300

    add signup page

--- app/src/main/java/com/example/tiki_taka/ui/SignupActivity.java
+++ app/src/main/java/com/example/tiki_taka/ui/SignupActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +17,10 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
     @BindView(R.id.login) TextView login;
 
+    private Button signup;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +30,22 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         login.setOnClickListener(this);
 
+        signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v == login){
+        if (v == login) {
 
             Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
             startActivity(intent);
 
-        }
+        } else if (v == signup) {
+            Intent intent = new Intent(SignupActivity.this, FirstActivity.class);
+            startActivity(intent);
 
+        }
     }
 }
