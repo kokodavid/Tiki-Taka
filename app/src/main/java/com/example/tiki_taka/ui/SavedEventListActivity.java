8217d8a7059ac5bd6a35a4adafde23676f309833
David mochoge <kingkgdave@gmail.com>
2019-06-02 12:44:30 +0300

    commit the changes to all neccesary files

--- app/src/main/java/com/example/tiki_taka/ui/SavedEventListActivity.java
+++ app/src/main/java/com/example/tiki_taka/ui/SavedEventListActivity.java
@@ -11,20 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tiki_taka.R;
-import com.example.tiki_taka.adapters.FirebaseEventListAdapter;
-import com.example.tiki_taka.adapters.FirebaseEventViewHolder;
-import com.example.tiki_taka.util.OnStartDragListener;
-import com.example.tiki_taka.util.SimpleItemTouchHelperCallback;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class SavedEventListActivity extends AppCompatActivity  {
 
@@ -37,7 +24,7 @@ public class SavedEventListActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_event_list);
+        setContentView(R.layout.activity_savedd_event_list);
        /* ButterKnife.bind(this);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
