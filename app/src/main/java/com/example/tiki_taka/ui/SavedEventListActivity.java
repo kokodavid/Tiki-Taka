88bf2fdee9d99c28f21a54fd84461c0ababab632
David mochoge <kingkgdave@gmail.com>
2019-06-05 12:02:10 +0300

    add camera functionality

--- app/src/main/java/com/example/tiki_taka/ui/SavedEventListActivity.java
+++ app/src/main/java/com/example/tiki_taka/ui/SavedEventListActivity.java
@@ -1,14 +1,8 @@
 package com.example.tiki_taka.ui;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+
 
 import com.example.tiki_taka.R;
 
@@ -16,22 +10,13 @@ import com.example.tiki_taka.R;
 public class SavedEventListActivity extends AppCompatActivity  {
 
 
-/*
-    RecyclerView RecyclerView;
-*/
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_savedd_event_list);
-       /* ButterKnife.bind(this);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid = user.getUid();
-
-        mEventReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_EVENTS).child(uid);
-        setUpFirebaseAdapter();*/
     }
 
 
